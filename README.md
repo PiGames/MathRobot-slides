9611f7f45958de4f2c0e24d88cf7bb304dc596ce
Bartosz Legięć <bartosz@legiec.eu>
2017-12-09 13:43:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Roboty i JavaScript czyli jak posypać ranę solą
+
+Bartek Legięć ([@bibix1999](http://twitter.com/bibix1999)) i Kacper Pietrzak ([@pietrzakacper](http://twitter.com/pietrzakacper))
---
+++ sketch.js
@@ -0,0 +1,40 @@
+let capture;
+let isVisible = true;
+let isFullscreen = false;
+
+function setup() {
+  capture = createCapture(VIDEO);
+  capture.size(300, 300 * (9/16));
+
+  capture.elt.style.width="300px"
+  capture.elt.style.height="auto"
+  noCanvas();
+}
+
+function keyPressed() {
+  if (keyCode === 81) {
+    isVisible = !isVisible;
+    if ( isVisible ) {
+      capture.play();
+      capture.elt.style.display="block";
+    } else {
+      capture.pause();
+      capture.elt.style.display="none";
+    }
+  }
+
+  if ( keyCode === 87 ) {
+    isFullscreen = !isFullscreen;
+    goFullScreen( isFullscreen );
+  }
+}
+
+function goFullScreen( isFullscreen ) {
+  if ( isFullscreen ) {
+    capture.elt.style.width="100%"
+    capture.elt.style.height="calc(100% - 10px)"
+  } else {
+    capture.elt.style.width="300px"
+    capture.elt.style.height="auto"
+  }
+}
